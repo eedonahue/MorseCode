40caaeb3bb4ff6dd702594d49f07cf2f87d18553
Ben Womble <69554103+BenWomble@users.noreply.github.com>
2022-06-07 10:46:54 +0200

    Update morse.py

--- morse.py
+++ morse.py
@@ -1,8 +1,8 @@
-#Program that encodes/decodes a message in morse code
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 
-#dictionary with key pair values for morse
-#probably a better way to write this
+
+
+# dictionary with key pair values for morse
 MORSE_CODE = { 'a': '.-',    'b': '-...',  'c': '-.-.',  'd': '-...',  'e': '.',
                'f': '..-.',  'g': '--.',   'h': '....',  'i': '..',    'j': '.----',
                'k': '-.-',   'l': '.-..',  'm': '--',    'n': '-.',    'o': '---',
@@ -33,11 +33,11 @@ def main():
       main()
 
 def encodeMorse(msg):
-    #make every character lowercase in the string
+    # make every character lowercase in the string
     msg = msg.lower()
     coded_msg = ''
 
-    #encode the message into morse
+    # encode the message into morse
     for character in msg:
         if character in MORSE_CODE:
             coded_msg += MORSE_CODE[character] + " "
@@ -52,22 +52,22 @@ def decodeMorse(msg):
     ENCODED_MORSE = {}
     to_replace = { "{": "", "}": "", "\'": ""}
 
-    #create a new dict with the key being dashes and dots
-    #and the value being the leter or number
+    # create a new dict with the key being dashes and dots
+    # and the value being the leter or number
     for key, value in MORSE_CODE.items():
         ENCODED_MORSE[value] = {key}
 
-    #if the morse code enter matches the dict the put the
-    #letter value into decoded_msg. ignore other chars that are
-    #not A-Z or 0-9
+    # if the morse code enter matches the dict the put the
+    # letter value into decoded_msg. ignore other chars that are
+    # not A-Z or 0-9
     for char in message:
         if char in ENCODED_MORSE:
             decoded_msg += str(ENCODED_MORSE[char])
         else:
             decoded_msg += char
 
-    #each letter is saved like this {'a'} so the unnecessary
-    #characters are removed - { } '
+    # each letter is saved like this {'a'} so the unnecessary
+    # characters are removed - { } '
     for key, value in to_replace.items():
         decoded_msg = decoded_msg.replace(key,value)
 
